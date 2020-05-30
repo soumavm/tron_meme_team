c6c23eb2dcc27cd088a0fc8e9abade98209d9960
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-05-30 18:10:28 -0400

    Added extra hidden layer to neural net
    Decreased mean squared error (MSE) from -90000 to -0.13, making it a viable model

--- ml.py
+++ ml.py
@@ -8,6 +8,11 @@ from sklearn.ensemble import RandomForestClassifier
 import random
 import seaborn as sns
 from sklearn.utils.multiclass import unique_labels
+from keras.wrappers.scikit_learn import KerasRegressor
+from keras.layers import Dense
+
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 
 from keras import layers, models
 import matplotlib.pyplot as plt
@@ -24,23 +29,22 @@ data.drop('% positive cases (may 25)', axis=1, inplace=True)
 
 x_train, x_test, y_train, y_test = train_test_split(data, target, test_size = 0.2)
 x_validate, x_test, y_validate, y_test = train_test_split(x_test, y_test, test_size=0.5)
-
-def random_forest():
-    clf = RandomForestClassifier(n_estimators = 60, max_depth = 4, min_samples_split = 0.01)
-    clf.fit(x_train, y_train)
-    y_pred = clf.predict(x_validate)
-    clf_accuracy = accuracy_score(y_validate, y_pred)
-    return clf_accuracy
-    
+x_train = x_train.astype(float)
+x_validate = x_validate.astype(float)
+x_test = x_test.astype(float)
+y_train = y_train.astype(float)
+y_validate = y_validate.astype(float)
+y_test = y_test.astype(float)
     
 def create_model():
     model = models.Sequential()
-    model.add(Dense(6, input_dim=6,kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1, kernel_initializer='normal'))
+    model.add(Dense(5, input_dim=5,kernel_initializer='normal', activation='relu'))
+    model.add(Dense(10, input_dim=5, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal', activation='relu'))
     model.compile(loss='mse', optimizer='adam')
     return model
 
-estimator = KerasRegressor(build_fn=create_model(), epochs=100, batch_size=5, verbose=0)
+estimator = KerasRegressor(build_fn=create_model, epochs=100, batch_size=5, verbose=0)
 kfold = KFold(n_splits=10)
-results=cross_val_score(estimator, x_validation, y_validation, cv=kfold)
+results=cross_val_score(estimator, x_test, y_test, cv=kfold)
 print("Baseline: %.2f (%.2f) MSE" % (results.mean(), results.std()))
